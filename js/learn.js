8fc9dd1023938d0d6d34f99a408de83eee1a55af
杨英承 <2689615085@qq.com>
2023-03-20 10:01:55 +0800

    [修改]添加js

--- js/learn.js
+++ js/learn.js
@@ -1,6 +1,15 @@
-let user = { name: '张三', age: 18 };
-let salary = { grade: "A" }
+// let user = { name: '张三', age: 18 };
+// let salary = { grade: "A" }
+//
+// let sum = { ...user, ...salary };
+//
+// console.log(sum);
 
-let sum = { ...user, ...salary };
+let a = "1a213";
+let b = +a;
 
-console.log(sum);
+console.log(typeof b)
+console.log(b)
+
+
+
